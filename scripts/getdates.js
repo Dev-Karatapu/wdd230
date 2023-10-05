e78ddce5dad54970e90379a8047e7b658644c764
Dev <samson.dev007@gmail.com>
2023-10-05 16:11:40 -0600

    "Completed Hambarger Menu"

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,21 +5,23 @@ document.getElementById('currentYear').textContent = currentYear;
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
 
-// // Store the selected elements that we are going to use. 
-// const mainnav = document.querySelector('.navigation')
-// const hambutton = document.querySelector('#menu');
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
 
-// // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-// hambutton.addEventListener('click', () => {
-// 	mainnav.classList.toggle('show');
-// 	hambutton.classList.toggle('show');
-// });
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
 
-// /* ❔What does toggle mean?
-// We could write separate add and remove statements. Toggle adds the class if it does not currently exist or removes the class if it does exist. 
-// The CSS class rules will handle the different views, layouts, and displays.
-// 🗝️ JavaScript only applies the class value or not.
-// */
+/* ❔What does toggle mean?
+We could write separate add and remove statements. Toggle adds the class if it does not currently exist or removes the class if it does exist. 
+The CSS class rules will handle the different views, layouts, and displays.
+🗝️ JavaScript only applies the class value or not.
+*/
+
+
 
 const menuButton = document.querySelector("#menu");
 
