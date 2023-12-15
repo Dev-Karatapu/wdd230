79720f9996f81342b0d9ae637f236ed3e4270c5e
Dev-Karatapu <devadanamkaratapu@gmail.com>
2023-12-15 00:58:23 -0700

    "More test push"

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,63 +1,92 @@
-const url1 = "data/members.json";
-const cards = document.querySelector("#cards");
+// directory.js
 
-async function getBusinessesData() {
-  const response = await fetch(url1);
-  const data = await response.json();
-  displayBusinesses(data.businesses);
-}
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function () {
+  const cardsContainer = document.getElementById('cards');
 
-const displayBusinesses = (businesses) => {
-  businesses.forEach((business) => {
-    let card = document.createElement("section");
-    let bName = document.createElement("h2");
-    let logo = document.createElement("img");
-    let memberSince = document.createElement("p");
-    let bDescription = document.createElement("p");
-    let address = document.createElement("p");
-    let website = document.createElement("a");
-    let membership = document.createElement("h4");
-
-    logo.setAttribute("src", business.logo);
-    logo.setAttribute("alt", `Logo of ${business.name}`);
-    logo.setAttribute("loading", "lazy");
-    logo.setAttribute("width", "auto");
-    logo.setAttribute("height", "auto");
-
-    bName.textContent = `${business.name}`;
-    bDescription.textContent = `${business.description}`;
-    address.textContent = `${business.address}`;
-    membership.textContent = `${business.membership} Membership`;
-    memberSince.textContent = `Member Since: ${business.since}`;
-    website.setAttribute("href", `${business.url}`);
-    website.textContent = business.url;
-    website.setAttribute("target", "_blank");
-
-    card.appendChild(logo);
-    card.appendChild(bName);
-    card.appendChild(bDescription);
-    card.appendChild(address);
-    card.appendChild(memberSince);
-    card.appendChild(membership);
-    card.appendChild(website);
-
-    cards.appendChild(card);
-  });
-};
-getBusinessesData();
-
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-gridbutton.addEventListener("click", () => {
-  display.classList.add("grid-directory");
-  display.classList.remove("list-directory");
+  // Fetch member data from members.json
+  fetch('data/members.json')
+      .then(response => response.json())
+      .then(data => {
+          data.forEach(member => {
+              // Create elements for member information
+              const card = document.createElement('div');
+              card.classList.add('member-card');
+              card.innerHTML = `
+                  <h2>${member.name}</h2>
+                  <p>${member.address}</p>
+                  <p>Phone: ${member.phone}</p>
+                  <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+                  <!-- Add more fields as needed -->
+              `;
+              cardsContainer.appendChild(card);
+          });
+      })
+      .catch(error => {
+          console.log('Error fetching member data:', error);
+      });
 });
 
-listbutton.addEventListener("click", showList);
+// const url1 = "data/members.json";
+// const cards = document.querySelector("#cards");
 
-function showList() {
-  display.classList.add("list-directory");
-  display.classList.remove("grid-directory");
-}
+// async function getBusinessesData() {
+//   const response = await fetch(url1);
+//   const data = await response.json();
+//   displayBusinesses(data.businesses);
+// }
+
+// const displayBusinesses = (businesses) => {
+//   businesses.forEach((business) => {
+//     let card = document.createElement("section");
+//     let bName = document.createElement("h2");
+//     let logo = document.createElement("img");
+//     let memberSince = document.createElement("p");
+//     let bDescription = document.createElement("p");
+//     let address = document.createElement("p");
+//     let website = document.createElement("a");
+//     let membership = document.createElement("h4");
+
+//     logo.setAttribute("src", business.logo);
+//     logo.setAttribute("alt", `Logo of ${business.name}`);
+//     logo.setAttribute("loading", "lazy");
+//     logo.setAttribute("width", "auto");
+//     logo.setAttribute("height", "auto");
+
+//     bName.textContent = `${business.name}`;
+//     bDescription.textContent = `${business.description}`;
+//     address.textContent = `${business.address}`;
+//     membership.textContent = `${business.membership} Membership`;
+//     memberSince.textContent = `Member Since: ${business.since}`;
+//     website.setAttribute("href", `${business.url}`);
+//     website.textContent = business.url;
+//     website.setAttribute("target", "_blank");
+
+//     card.appendChild(logo);
+//     card.appendChild(bName);
+//     card.appendChild(bDescription);
+//     card.appendChild(address);
+//     card.appendChild(memberSince);
+//     card.appendChild(membership);
+//     card.appendChild(website);
+
+//     cards.appendChild(card);
+//   });
+// };
+// getBusinessesData();
+
+// const gridbutton = document.querySelector("#grid");
+// const listbutton = document.querySelector("#list");
+// const display = document.querySelector("article");
+
+// gridbutton.addEventListener("click", () => {
+//   display.classList.add("grid-directory");
+//   display.classList.remove("list-directory");
+// });
+
+// listbutton.addEventListener("click", showList);
+
+// function showList() {
+//   display.classList.add("list-directory");
+//   display.classList.remove("grid-directory");
+// }
